3c3e6925958c08e055c9471e1d0479b8d6225020
Akshat Pandey <thatweirdakshat@gmail.com>
2025-06-16 22:48:48 +0530

    fixed the expanded widget bug

--- frontend/lib/time_tracking/widgets/time_entry_widget.dart
+++ frontend/lib/time_tracking/widgets/time_entry_widget.dart
@@ -2,11 +2,11 @@ import 'package:flutter/material.dart';
 import 'package:frontend/time_tracking/entities/time_entry.dart';
 
 class TimeEntryWidget extends StatefulWidget {
-  final BuildContext context;
+  // final BuildContext context;
   final TimeEntry timeEntry;
   const TimeEntryWidget({
     required this.timeEntry,
-    required this.context,
+    // required this.context,
     super.key,
   });
 
@@ -17,44 +17,43 @@ class TimeEntryWidget extends StatefulWidget {
 class _TimeEntryWidgetState extends State<TimeEntryWidget> {
   @override
   Widget build(BuildContext context) {
-    return LayoutBuilder(
-      builder: (context, constraints) => Column(
+    return Padding(
+      padding: const EdgeInsets.all(8.0),
+      child: Column(
         crossAxisAlignment: CrossAxisAlignment.center,
         children: [
-          Center(
-            child: Container(
-                decoration: BoxDecoration(
-                    borderRadius: BorderRadius.circular(10),
-                    color: Color(0xff1E1E1E)),
-                width: constraints.maxWidth * .95,
-                height: constraints.maxHeight * (0.1),
-                child: Padding(
-                  padding: const EdgeInsets.all(8.0),
-                  child: Column(
-                    crossAxisAlignment: CrossAxisAlignment.end,
-                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                    children: [
-                      Row(
-                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                        children: [
-                          Text(widget.timeEntry.description),
-                          (Text(widget.timeEntry.duration
-                              .toString()
-                              .split('.')
-                              .first)),
-                        ],
-                      ),
-                      Row(
-                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                        children: [
-                          Text(widget.timeEntry.categoryName),
-                          Icon(Icons.play_arrow),
-                        ],
-                      )
-                    ],
-                  ),
-                )),
-          )
+          Container(
+              decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(10),
+                  color: Color(0xff1E1E1E)),
+              width: MediaQuery.of(context).size.width,
+              height: MediaQuery.of(context).size.height * .10,
+              child: Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.end,
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        Text(widget.timeEntry.description),
+                        (Text(widget.timeEntry.duration
+                            .toString()
+                            .split('.')
+                            .first)),
+                      ],
+                    ),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        Text(widget.timeEntry.categoryName),
+                        Icon(Icons.play_arrow),
+                      ],
+                    )
+                  ],
+                ),
+              ))
         ],
       ),
     );
