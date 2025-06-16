6ad19662620a77ffedce2092c40c330b2f84dba0
Akshat Pandey <thatweirdakshat@gmail.com>
2025-06-16 22:47:42 +0530

    added the day_timeline_widget (time entries for each day)

--- frontend/lib/time_tracking/widgets/day_timeline_widget.dart
+++ frontend/lib/time_tracking/widgets/day_timeline_widget.dart
@@ -1,13 +1,33 @@
 import 'package:flutter/material.dart';
+import 'package:frontend/time_tracking/entities/time_entry.dart';
+import 'package:frontend/time_tracking/widgets/time_entry_widget.dart';
 
 class DayTimelineWidget extends StatelessWidget {
-  const DayTimelineWidget({super.key});
+  const DayTimelineWidget({super.key, required this.date, required this.list});
+  final DateTime date;
+  final List<TimeEntry> list;
 
   @override
-  Widget build(
-    BuildContext context,
-  ) {
-    return ListView.builder(
-        itemCount: 1, itemBuilder: (context, index) => Text('data'));
+  Widget build(BuildContext context) {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: Text("${date.day}/${date.month}/${date.year}"),
+        ),
+        ListView.builder(
+            shrinkWrap: true,
+            physics: NeverScrollableScrollPhysics(),
+            itemCount: list.length,
+            itemBuilder: (context, index) {
+              // print(list);
+              return TimeEntryWidget(
+                timeEntry: list[index],
+                // context: context,
+              );
+            })
+      ],
+    );
   }
 }
