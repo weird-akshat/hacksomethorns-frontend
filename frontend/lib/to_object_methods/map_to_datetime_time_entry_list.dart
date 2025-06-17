a6008280653c0ed6518d7f3d7d0bedafa85fa696
Akshat Pandey <thatweirdakshat@gmail.com>
2025-06-18 04:07:08 +0530

    time tracking integrated, (raat me commitment se baat karne ki umar me code commit kar rha hoon, sad life)

--- frontend/lib/to_object_methods/map_to_datetime_time_entry_list.dart
+++ frontend/lib/to_object_methods/map_to_datetime_time_entry_list.dart
@@ -2,13 +2,14 @@ import 'package:frontend/time_tracking/entities/time_entry.dart';
 import 'package:frontend/to_object_methods/map_to_time_entry.dart';
 
 Map<DateTime, List<TimeEntry>> mapToDateTimeTimeEntryList(
-    Map<String, List<Map<String, dynamic>>> rawData) {
+    Map<String, dynamic> rawData) {
   final Map<DateTime, List<TimeEntry>> result = {};
 
   rawData.forEach((dateStr, entryList) {
     final date = DateTime.parse(dateStr);
-    final timeEntries =
-        entryList.map((entry) => mapToTimeEntry(entry)).toList();
+    final timeEntries = (entryList as List<dynamic>)
+        .map((entry) => mapToTimeEntry(entry))
+        .toList();
     result[date] = timeEntries;
   });
 
